dd9d2b252cff2ad1a95a60d7929c5a5247c0b242
devdave <devdave@ominian.net>
2022-09-21 10:02:28 -0600

    Build out the base test_float compatible tokenizibg logic

--- src/lexer/tokenizer.rs
+++ src/lexer/tokenizer.rs
@@ -64,6 +64,44 @@ impl Tokenizer {
 
         for (lineno, line,) in source.into_iter().enumerate() {
             println!("Parsing {}-`{:?}`", lineno, line);
+    fn process_line(&self, state: &State, lineno: usize, line: String) -> Result<Vec<Token>, TokError> {
+        let mut product: Vec<Token> = Vec::new();
+        println!("Parsing {}-`{:?}`", lineno, line);
+
+        let mut code = CodeLine::new(line);
+
+
+        while code.remaining() > 0 {
+            let col_pos = code.position();
+            if let Some((new_pos, found)) = code.return_match(POSSIBLE_NAME.to_owned()) {
+                product.push(Token::quick(TType::Name, lineno, col_pos, new_pos, found));
+
+            } else if let Some((new_pos, found)) = code.return_match(POSSIBLE_ONE_CHAR_NAME.to_owned()) {
+                product.push(Token::quick(TType::Name, lineno, col_pos, new_pos, found));
+
+            }
+            else if let Some((new_pos, found)) = code.return_match(FLOATING_POINT.to_owned()) {
+                product.push(Token::quick(TType::Number, lineno, col_pos, new_pos, found));
+            }
+            else if let Some((new_pos, found)) = code.return_match(OPERATOR_RE.to_owned()) {
+                product.push(Token::quick(TType::Op, lineno, col_pos, new_pos, found));
+            }
+            //Look for WS
+            else if let Some((new_pos, found)) = code.return_match(SPACE_TAB_FORMFEED_RE.to_owned()) {
+                //and ignore it
+            }
+            else {
+                if let Some(sym) = code.get() {
+                    if sym == " " {
+                        //skipping white space
+                    } else if sym == "\n" {
+                        product.push(Token::quick(TType::NL, lineno, col_pos, code.position(), "\n".to_string()));
+                        break;
+                    }
+                }
+                break;
+            }
+
         }
 
         return Ok(product);
