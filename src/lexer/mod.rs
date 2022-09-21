4c0af48ad3f0a7e848fd62d7604f1f2d7d97034f
devdave <devdave@ominian.net>
2022-09-21 10:03:21 -0600

    expose tokenizer and config struct

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,2 +1,6 @@
-mod tokenizer;
-mod ws_cleaner;
+pub mod tokenizer;
+pub mod ws_cleaner;
+mod code_line;
+
+pub use super::lexer::tokenizer::{TConfig, Tokenizer};
+pub use super::lexer::ws_cleaner::cleaner;
