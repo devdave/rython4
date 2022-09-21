8b554fc693be63ae955dfba04755f57d63b32b90
devdave <devdave@ominian.net>
2022-09-21 10:03:46 -0600

    expose patterns module and bring common structs up

--- src/tokens/mod.rs
+++ src/tokens/mod.rs
@@ -1,5 +1,5 @@
 mod operators;
-mod patterns;
+pub mod patterns;
 mod position;
 pub mod processor;
 mod token;
@@ -9,3 +9,5 @@ mod error;
 pub use super::tokens::position::Position;
 pub use super::tokens::token::Token;
 pub use super::tokens::error::TokError;
+pub use super::tokens::ttype::TType;
+pub use super::tokens::operators::OPERATOR_RE;
