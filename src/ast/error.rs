256dbfe6c9dcaa9ce3e5f51199843012e23d053c
devdave <devdave@ominian.net>
2022-09-28 13:17:10 -0600

    Fix for new paths/locations and gut out lifetime

--- src/ast/error.rs
+++ src/ast/error.rs
@@ -1,18 +1,18 @@
 
-use crate::tokenizer::TokError;
+use crate::tokens::TokError;
 use crate::parser::TokVec;
 use peg::Parse;
 use thiserror::Error;
 
 #[allow(clippy::enum_variant_names)]
 #[derive(Debug, Error, PartialEq, Eq)]
-pub enum ParserError<'a> {
+pub enum ParserError {
     #[error("tokenizer error: {0}")]
-    TokenizerError(TokError, &'a str),
+    TokenizerError(TokError, String),
     #[error("parser error: {0}")]
     ParserError(
-        peg::error::ParseError<<TokVec<'a> as Parse>::PositionRepr>,
-        &'a str,
+        peg::error::ParseError<<TokVec as Parse>::PositionRepr>,
+        String,
     ),
     #[error("invalid operator")]
     OperatorError,
