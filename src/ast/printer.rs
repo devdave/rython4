915e21437c55ad759cfdc70e72fd2dc75de40f55
devdave <devdave@ominian.net>
2022-10-05 14:22:27 -0600

    Oh my god do I have a LOT of typing to do :(

--- src/ast/printer.rs
+++ src/ast/printer.rs
@@ -1,15 +1,27 @@
+use crate::ast::{Expr, Expression, SmallStatement};
 use super::module::Module;
 use super::statement::{
     Statement, SimpleStatementLine
 };
 
+fn gen_depth_string(depth: usize) -> String {
+    let mut depth_string: String = String::new();
+    for i in 1..depth {
+        depth_string.push_str("\t");
+    }
+
+    return depth_string;
+}
+
 pub fn print_module(module:Module) {
 
-    println!("Statements: {}", module.body.len());
+    let depth = 0;
+
+    println!("Module statements: {}", module.body.len());
     for element in module.body {
         match element {
             Statement::Simple(statement_line) => {
-                print_statementline(statement_line);
+                print_statementline(statement_line, depth + 1);
             },
             Statement::Compound(compound_statements) => {
                 println!("Compound {:?}", compound_statements);
@@ -20,9 +32,67 @@ pub fn print_module(module:Module) {
 
 }
 
-fn print_statementline(line: SimpleStatementLine) {
-    println!("Simple statement: ");
+fn print_statementline(line: SimpleStatementLine, depth: usize) {
+    println!("{}Simple statement: ", gen_depth_string(depth));
+
     for element in line.body {
-        println!("\tExpression: {:?}", element);
+        match element {
+            SmallStatement::Expr(expr) => {
+                print_expr(expr, depth + 1);
+            },
+            _ => {
+                println!("{}Unhandled: {:?}", gen_depth_string(depth+1), element);
+            }
+        }
     }
+}
+
+fn print_expr(expr: Expr, depth: usize) {
+
+    println!("{}Expression: ", gen_depth_string(depth));
+    let body = expr.value;
+
+    match body {
+        Expression::Name(val) => {
+            println!("{}Name:{:?}", gen_depth_string(depth+1), val);
+        }
+        Expression::Ellipsis => {
+            println!("{}Ellipsis", gen_depth_string(depth+1));
+        }
+        Expression::Integer(val) => {
+            println!("{}Integer:{:?}", gen_depth_string(depth+1), val);
+        }
+        Expression::Float(val) => {
+            println!("{}Float:{:?}", gen_depth_string(depth+1), val);
+        }
+        Expression::Binary(_) => {}
+        Expression::Hexidecimal(_) => {}
+        Expression::Imaginary(_) => {}
+        Expression::Comparison(_) => {}
+        Expression::UnaryOperation(_) => {}
+        Expression::BinaryOperation(_) => {}
+        Expression::BooleanOperation(_) => {}
+        Expression::Attribute(_) => {}
+        Expression::Tuple(_) => {}
+        Expression::Call(_) => {}
+        Expression::GeneratorExp(_) => {}
+        Expression::ListComp(_) => {}
+        Expression::SetComp(_) => {}
+        Expression::DictComp(_) => {}
+        Expression::List(_) => {}
+        Expression::Set(_) => {}
+        Expression::Dict(_) => {}
+        Expression::Subscript(_) => {}
+        Expression::StarredElement(_) => {}
+        Expression::IfExp(_) => {}
+        Expression::Lambda(_) => {}
+        Expression::Yield(_) => {}
+        Expression::Await(_) => {}
+        Expression::SimpleString(_) => {}
+        Expression::ConcatenatedString(_) => {}
+        Expression::FormattedString(_) => {}
+        Expression::NamedExpr(_) => {}
+    }
+
+
 }
