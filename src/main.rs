67542085a1939d459071881d9ac31657a8af30d0
devdave <devdave@ominian.net>
2022-10-24 19:11:53 -0600

    For now skip the encoding token

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,7 @@ fn py_run_file(filename: PathBuf, show_tokens: bool)  {
 
 
     let mut lines = cleaner(buffer);
-    let mut tokenizer = Tokenizer::new(TConfig{ skip_encoding: false, skip_endmarker: false } );
+    let mut tokenizer = Tokenizer::new(TConfig{ skip_encoding: true, skip_endmarker: false } );
     let outcome = tokenizer.generate(lines);
     if let Ok(tokens) = outcome {
         if show_tokens == true {
