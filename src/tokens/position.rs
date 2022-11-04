ace718f9576cd21206381cde7b5468cb3f271aba
devdave <devdave@ominian.net>
2022-11-04 11:06:21 -0600

    Change the order of how position's properties are printed

--- src/tokens/position.rs
+++ src/tokens/position.rs
@@ -17,6 +17,8 @@ impl Position {
     pub fn t((col, line): (usize, usize)) -> Self {
         Self::m(col, line)
     }
+
+    pub fn t2((line, col): (usize, usize)) -> Self{ Self::m(col, line)}
 }
 
 impl PartialEq for Position {
@@ -34,8 +36,8 @@ impl PartialEq for Position {
 impl Debug for Position {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("Position")
-            .field("col", &self.col)
             .field("line", &self.line)
+            .field("col", &self.col)
             .finish()
     }
 }
