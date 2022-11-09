caa27f39caf3635c169058ad5c142d8473e355a6
devdave <devdave@ominian.net>
2022-11-09 11:20:17 -0700

    Stop freaking out when we're done.

--- src/lexer/tokenizer.rs
+++ src/lexer/tokenizer.rs
@@ -211,7 +211,8 @@ impl Tokenizer {
                 },
                 _ => {
                     //Err how did we get here?
-                    panic!("How did we get here? {:?} @ {:#?}", code.line, state);
+                    // panic!("How did we get here? {:?} @ {:#?}", code.line, state);
+                    return Ok(Some(found));
                 }
             }
         }
