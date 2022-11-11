ebdb25328434d4201651d36e9293bf916aa40f1f
devdave <devdave@ominian.net>
2022-11-11 13:37:48 -0700

    expose operators

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -2,6 +2,7 @@ pub mod tokenizer;
 pub mod ws_cleaner;
 mod code_line;
 mod tests;
+mod operators;
 
 pub use super::lexer::tokenizer::{TConfig, Tokenizer};
 pub use super::lexer::ws_cleaner::cleaner;
